356f80e322bafe4500de58f1ad4ccf58063e1292
Mirko Tebaldi <mirko.tebaldi@diemmea.com>
2024-07-05 08:41:56 +0200

    struttura delle card in homepage pronta

--- components/card/CountryFlagAndName.tsx
+++ components/card/CountryFlagAndName.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { findCountryByCode } from '@/utils/countries';
 
-function CountryFlagAndName() {
+function CountryFlagAndName({ countryCode }: { countryCode: string }) {
+  const validCountry = findCountryByCode(countryCode);
+  const countryName =
+    validCountry!.name.length > 20
+      ? `${validCountry!.name.substring(0, 20)}...`
+      : validCountry!.name;
   return (
-    <div>
-      CountryFlagAndName
-    </div>
-  )
+    <span className='flex justify-between items-center gap-2 text-sm '>
+      {validCountry?.flag} {countryName}
+    </span>
+  );
 }
-
-export default CountryFlagAndName
+export default CountryFlagAndName;
--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-
-function FavoriteToggleButton() {
+import { FaHeart } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
   return (
-    <div>
-      FavoriteToggleButton
-    </div>
-  )
+    <Button size="icon" variant="outline" className="p-2 cursor-pointer">
+      <FaHeart />
+    </Button>
+  );
 }
-
-export default FavoriteToggleButton
+export default FavoriteToggleButton;
