5ea750c1cac1c4c0fe07790488e5b6d6f54768eb
Mirko Tebaldi <mirko.tebaldi@diemmea.com>
2024-07-05 09:05:25 +0200

    search is working !

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,11 +1,44 @@
+"use client";
+
 import { Input } from "../ui/input";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+import { useState, useEffect } from "react";
 
 function NavSearch() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const [search, setSearch] = useState(searchParams.get("search")?.toString());
+
+  // questa tecnica gestisce eventuali altri parametri di cui non sono a conoscenza
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [searchParams.get("search")]);
+
   return (
     <Input
       type="search"
       placeholder="find a property..."
       className="max-w-xs dark:bg-muted "
+      value={search}
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
     />
   );
 }
