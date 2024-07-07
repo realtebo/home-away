c815bfecb9b81b667ab1a6f6c31654b499583e07
Mirko Tebaldi <mirko.tebaldi@diemmea.com>
2024-07-07 19:01:37 +0200

    fix navsearch path replacement when searching

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { Input } from "../ui/input";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 import { useState, useEffect } from "react";
 
 function NavSearch() {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const { replace } = useRouter();
 
   const [search, setSearch] = useState(searchParams.get("search")?.toString());
@@ -20,7 +19,9 @@ function NavSearch() {
     } else {
       params.delete("search");
     }
-    replace(`${pathname}?${params.toString()}`);
+    // only home page as logic to handle search, so we 
+    // replace at all the pathname (this is a real redirect !)
+    replace(`/?${params.toString()}`);
   }, 500);
 
   useEffect(() => {
